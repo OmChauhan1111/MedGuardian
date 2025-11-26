b9f7b548e4a952409314e7dc422c3b726839e20e
Om Singh Chauhan <omchauhanom1111@gmail.com>
2025-11-27 01:22:07 +0530

    Update database configuration for remote MySQL server

--- db.py
+++ db.py
@@ -6,10 +6,10 @@ from typing import List, Dict, Any
 
 # ---------- Update these if your XAMPP MySQL has password / different user ----------
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",        # <-- put your MySQL password if any
-    "database": "medguardian",
+    "host": "sql313.infinityfree.com",
+    "user": "if0_40528967",
+    "password": "wlXeL06y1w",        # <-- put your MySQL password if any
+    "database": "if0_40528967_medguardian",
     "raise_on_warnings": True,
     "autocommit": False
 }
